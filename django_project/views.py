f4bff1dc46b8d158768939cbff00430ab90100c3
Russell wangila <135205866+Russell-sam@users.noreply.github.com>
2024-04-24 11:15:22 +0000

    first commit

--- django_project/views.py
+++ django_project/views.py
@@ -3,7 +3,19 @@ import requests
 from django.shortcuts import render
 
 def index(request):
-  r1 = request.GET.get('https://uselessfacts.jsph.pl/random.json?language=en')
-  data = r1.json()
+  response = requests.get('https://uselessfacts.jsph.pl/random.json?language=en')
+  data = response.json()
   fact = data['text']
-  return render(request, 'templates/index.html', {'fact': fact})
+
+  r3 = requests.get('https://dog.ceo/api/breeds/image/random')
+  res3 = r3.json()
+  dog = res3['message']
+
+  # This is the assignment for the Hackathon, 
+  # Instructions: 
+  # Use this API and randomize the students
+  response2 = requests.get('https://freetestapi.com/api/v1/students') # Use this API
+  data2 = response2.json()
+  name = data2[0]['name']
+
+  return render(request, 'templates/index.html', {'fact': fact, 'dog': dog,  'name': name})
